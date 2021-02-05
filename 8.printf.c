69bdf2a177e0d553d2f29ea01df622959d1ce0a8
Glenn Ou <2738328919@qq.com>
2021-02-06 01:29:10 +0800

    Add files via upload

---
+++ 8.printf.c
@@ -0,0 +1,22 @@
+/*
+To Do List:
+1.Brief the content of the video;
+2.Write the program in the codeblock;
+3.printf can print a short text;
+4.printf can print a insert a text;
+5.printf can print a varible text;
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int year = 2021;
+printf("Hello World\n");
+printf("Hello %s\n", "world");
+printf("Hello %d\n", year);
+
+return 0;
+}
+
+

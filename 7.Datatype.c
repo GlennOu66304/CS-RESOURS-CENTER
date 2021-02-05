e6769ec8919b7e2e10012d84e2e7b9c2be32c9ae
Glenn Ou <2738328919@qq.com>
2021-02-06 01:08:51 +0800

    Add files via upload

---
+++ 7.Datatype.c
@@ -0,0 +1,20 @@
+/*
+To Do List:
+1.List the data type of the C;
+2.Write the program in the codeblock;
+3.integer, flote, character, string;
+4.integer, flote, character could be modified, and the string can not be mdified;
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int age = 40;
+double gpa = 3.6;
+char grade = 'A';
+char phrase[] = "Giraffe Academy"
+
+
+return 0;
+}

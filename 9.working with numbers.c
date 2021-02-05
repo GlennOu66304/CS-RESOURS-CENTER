7f70b98bd002ae2887106da0c12d1f0969c45ff2
Glenn Ou <2738328919@qq.com>
2021-02-06 01:45:51 +0800

    Add files via upload

---
+++ 9.working with numbers.c
@@ -0,0 +1,23 @@
+/*
+To Do List:
+1.Brief the content of the video;
+2.Write the program in the codeblock;
+3.Given a number in the printf
+4.Numbers operation
+5.Working on with the math function
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("%f \n", 6.66);
+printf("%f \n", 5.55 + 6.66);
+printf("%f \n", pow(2,3));
+printf("%f \n", sqrt(36));
+
+return 0;
+}
+
+

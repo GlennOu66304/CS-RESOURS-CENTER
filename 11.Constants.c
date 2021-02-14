047e8de4831243abbeaf9b39fc7127f664343329
Glenn Ou <2738328919@qq.com>
2021-02-14 21:17:42 +0800

    Add files via upload

---
+++ 11.Constants.c
@@ -0,0 +1,29 @@
+/*
+To Do List:
+1.Brief the content of the video;
+2.Write the program in the codeblock;
+*/
+
+
+/*
+To do list:
+1.constant means that the value you can not change;
+2. few situation: you can not change the value of the code;
+3. write those code in the below and update it to the github
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+ int num = 5;
+
+printf("%d\n",num);
+num = 8;
+printf("%d",num);
+return 0;
+}
+
+
